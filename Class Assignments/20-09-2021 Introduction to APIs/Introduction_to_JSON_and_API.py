84792f072de1319cbfed7a4cbfa8556f86d88f0e
SnehithReddy09 <67829978+SnehithReddy09@users.noreply.github.com>
2021-09-30 22:24:30 +0530

    Add files via upload

---
+++ Class Assignments/20-09-2021 Introduction to APIs/Introduction_to_JSON_and_API.py
@@ -0,0 +1,56 @@
+'''
+PROGRAM DESCRIPITON:
+	Creating a json file and readin from it.
+    Checking the URL and Retrieving data from URL and print details of "Afghan Hound". 
+'''
+
+# PROGRAMMED BY: PULI SNEHITH REDDY
+# MAIL ID : snehithreddyp@gmail.com
+# DATE    : 20-09-2021
+# VERSION : 3.7.9
+# CAVEATS : None
+# LICENSE : None
+
+
+
+import requests
+import json
+
+class json1:
+    def create_json(self, data):
+        with open("welcome.json", "w") as file:
+            return json.dump(data, file)
+    
+    def read_json(self, file):
+        with open(file)as file:
+            return json.load(file)
+
+    def url_check(self,url):
+        try:
+            url=requests.get(url)
+            return True
+        except:
+            return False
+    def read_url(self,url):
+        url = requests.get(url)
+        return url.json()
+
+url="https://api.thedogapi.com/v1/breeds"
+
+a=json1()
+#checking url is present or not. Enters if True.
+if a.url_check(url):
+    #reading data from URL and json file is returned
+    x=a.read_url(url)
+    #iterating the json file
+    for i in x:
+        if 'name' in i:
+            #Checking name and if matches then printing details.
+            if i['name']=="Afghan Hound":
+                print("Found")
+                print(i)
+                break
+    else:
+        print("Not found")
+
+    
