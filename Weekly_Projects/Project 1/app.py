9967e5c8bf840e5fdb2a46bf6ef3aadb0a5ac985
SnehithReddy09 <67829978+SnehithReddy09@users.noreply.github.com>
2021-10-01 08:37:04 +0530

    Update app.py

--- Weekly_Projects/Project 1/app.py
+++ Weekly_Projects/Project 1/app.py
@@ -1,8 +1,5 @@
 '''PROGRAM DESCRIPITON: 
-	Create a RESTFUL API Server in Python Flask. To achieve your target go through following process:-
-    '''PROGRAM DESCRIPITON: 
-	Create a RESTFUL API Server in Python Flask. To achieve your target go through following process:-
-    
+    Create a RESTFUL API Server in Python Flask. To achieve your target go through following process:-
     1) You have to hit the URL "https://api.thedogapi.com/v1/breeds" into a local JSON file into ur localhost.
     2) From the JSON file scrap the data of the breed of dog, country of origin,
         bred for which purpose and the image of the dog.
@@ -10,29 +7,8 @@
     4) Send the extracted data into a MongoDB database with basic CRUD operations associated with it.
 	
  
-    In this 1,2,4 are present. 
+    In this 1,2,4 are present . 
 '''
-
-# PROGRAMMED BY: PULI SNEHITH REDDY
-# MAIL ID : snehithreddyp@gmail.com
-# DATE    : 27-09-2021
-# VERSION : 3.7.9
-# CAVEATS : None
-# LICENSE : None
-
-    1) You have to hit the URL "https://api.thedogapi.com/v1/breeds" into a local JSON file into ur localhost.
-    2) From the JSON file scrap the data of the breed of dog, country of origin,
-        bred for which purpose and the image of the dog.
-    3) Display the data in a tabular format into a HTML page.
-    4) Send the extracted data into a MongoDB database with basic CRUD operations associated with it.
-'''
-
-# PROGRAMMED BY: PULI SNEHITH REDDY
-# MAIL ID : snehithreddyp@gmail.com
-# DATE    : 27-09-2021
-# VERSION : 3.7.9
-# CAVEATS : None
-# LICENSE : None
 
 import requests
 import json 
