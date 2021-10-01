b4301780f14bfe9e78e778b20b5a968ae0c058c9
SnehithReddy09 <67829978+SnehithReddy09@users.noreply.github.com>
2021-10-01 08:34:59 +0530

    Update app.py

--- Weekly_Projects/Project 1/app.py
+++ Weekly_Projects/Project 1/app.py
@@ -46,26 +46,33 @@ class func:
         #Retrieving data from URL using requests.get() and then returning it by converting into json object.
         url = requests.get(url)
         return url.json()
-        
+ 
 class One:
+	
+    #Establishment of connection with MongoDB.
     connection = MongoClient("mongodb://localhost:27017")
 
     def mongo_connection(self):
+	#If connection is made True is returned else False.
         if self.connection:
             return True 
         else:
             return False 
+    
     def mongodb_list(self):
+	#checking whether connection is done and if True return list of Database Names.
         if self.mongo_connection() == True:
             return self.connection.list_database_names()
 
     def db_exists(self, db_name):
+	#Return True if particular database name exists.
         if db_name in self.mongodb_list():
             return True
         else:
             return False 
     
     def create_new_collection(self, db_name, new_collection):
+	#If connection is done succesfully new_collection is created and returned.
         if self.connection:
             db_name = self.connection[db_name]
             new_collection = db_name[new_collection]
@@ -73,23 +80,31 @@ class One:
         else:
             return("error")
     
+    # timestand for mongodb 
+    def timestamp():
+        import datetime as dt 
+        return dt.datetime.now()
+    
     
     def insert_data(self,db_name,collection_name,data):
         if self.connection:
+	    #Inserting the data into database where database name="db_name" and collection is "collection_name"
             self.connection[db_name][collection_name].insert_one(data)
             return "success"
         else:
             return "error"
     
-    def display(self,db_name,collection_name):
+     def display(self,db_name,collection_name):
         a=[]
         if self.connection:
+	    #Iterating the documents in collection and appending into list.
             for i in self.connection[db_name][collection_name].find():
                 a.append(i)
-               
+		
+            #Iterating list and displaying contents in it.
             for i in a:
                 print(i)
-                print("..............")    
+                print("..............")
    
 #url used for retrieving data from the web
 url = "https://api.thedogapi.com/v1/breeds"
